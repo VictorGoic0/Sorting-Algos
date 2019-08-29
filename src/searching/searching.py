3b3edbc5e5444ce24d7e07034d94b676eb3737d1
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-08-29 16:41:51 -0400

    Binary search function completed.

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -9,16 +9,20 @@ def linear_search(arr, target):
 
 # STRETCH: write an iterative implementation of Binary Search 
 def binary_search(arr, target):
-
   if len(arr) == 0:
     return -1 # array empty
-    
-  low = 0
-  high = len(arr)-1
-
-  # TO-DO: add missing code
-
-  return -1 # not found
+  left = 0
+  right = len(arr) - 1
+  while left <= right:
+    midpoint = (left + right) // 2
+    if arr[midpoint] == target:
+      return midpoint
+    else:
+      if target < arr[midpoint]:
+        right = midpoint - 1
+      else:
+        left = midpoint+1
+  return -1
 
 
 # STRETCH: write a recursive implementation of Binary Search 
