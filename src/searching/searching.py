133cd331f55f57ce334aafeaedfd01e3ac2c0a44
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-08-29 16:55:04 -0400

    Recursive binary search algorithm completed.

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -21,15 +21,22 @@ def binary_search(arr, target):
       if target < arr[midpoint]:
         right = midpoint - 1
       else:
-        left = midpoint+1
+        left = midpoint + 1
   return -1
 
 
 # STRETCH: write a recursive implementation of Binary Search 
 def binary_search_recursive(arr, target, low, high):
-  
-  middle = (low+high)//2
-
+  midpoint = (low + high) // 2
   if len(arr) == 0:
     return -1 # array empty
-  # TO-DO: add missing if/else statements, recursive calls
+  else:
+    if arr[midpoint] == target:
+      return midpoint
+    elif low > high:
+      return -1
+    else:
+      if target < arr[midpoint]:
+        return binary_search_recursive(arr, target, low, midpoint - 1)
+      else:
+        return binary_search_recursive(arr, target, midpoint + 1, high)
